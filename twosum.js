a22fe5775357487e010b9b232e8f8938b46c05f2
brazz7762 <rasmussen.benjaminj@gmail.com>
2021-01-26 11:53:55 -0600

    Update to twoSums

---
+++ twosum.js
@@ -0,0 +1,53 @@
+// /**
+//  * @param {number[]} nums
+//  * @param {number} target
+//  * @return {number[]}
+//  */
+
+var twoSum = function(nums, target) {
+  var begin = 0;
+  var outArr = [];
+  while(begin < nums.length - 1){
+    var start = nums[begin];
+    for(var i = 1; i < nums.length; i++) {
+        console.log('nums at I', i)
+      if((start + nums[i]) === target) {
+        outArr.push(begin)
+        outArr.push(i)
+        return outArr;
+      }
+    }
+    begin ++;
+  }
+  return outArr;
+};
+
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+
+
+
+// Example 1:
+
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+// Example 2:
+
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
+// Example 3:
+
+// Input: nums = [3,3], target = 6
+// Output: [0,1]
+
+
+// Constraints:
+
+// 2 <= nums.length <= 103
+// -109 <= nums[i] <= 109
+// -109 <= target <= 109
+// Only one valid answer exists.
